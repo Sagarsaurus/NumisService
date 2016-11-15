838d3df5d7f1e5001a3fa6308b4b43545ad26e14
Abhishek Deo <Abhishek_Deo@msn.com>
2016-11-14 23:15:24 -0500

    connection to db

--- src/main/java/io/numis/service/App.java
+++ src/main/java/io/numis/service/App.java
@@ -1,11 +1,22 @@
 package io.numis.service;
 
-/**
- * Hello world!
- *
- */
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Exception {
+    	Connection connection = getConnection();
+    }
+    
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+    	String testURI = "";
+    	URI numisDbUri = new URI(System.getenv(testURI));
+    	
+    	String username = numisDbUri.getUserInfo().split(":")[0];
+    	String password = numisDbUri.getUserInfo().split(":")[1];
+    	String numisDbUrl = "jdbc:postgresql://" + numisDbUri.getHost() + numisDbUri.getPath();
+    	
+    	return DriverManager.getConnection(numisDbUrl, username, password);
     }
 }
