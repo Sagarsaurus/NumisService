6c571c8faa0778988cb9f4f289c1e7dc90419036
Karan Pahawa <kpahawa@gmail.com>
2016-11-17 21:29:18 -0500

    added web.xml to configure the .war file.

---
+++ src/main/java/presentation/NumisInitServlet.java
@@ -0,0 +1,15 @@
+package presentation;
+
+import java.io.UncheckedIOException;
+
+import javax.servlet.http.HttpServlet;
+
+public class NumisInitServlet {
+//	static {
+//		try {
+//			new XMLPersistenceLoader("/dao.xml").loadValues();
+//		} catch (Exception e) {
+//			throw new Exception(e);
+//		}		
+//	}
+}
